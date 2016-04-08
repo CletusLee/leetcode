e532cf1d9c10ee3707e386f8ae2c0d9693873d2e
Cletus <clet.lee@gmail.com>
2016-04-08 23:46:46 +0800

    setup the first test

---
+++ leetcode/src/test/java/leetcode/questions/TestTwoSum.java
@@ -0,0 +1,13 @@
+package leetcode.questions;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+public class TestTwoSum {
+
+	@Test
+	public void firstTest() {
+		assertThat(1, is(1));
+	}
+}
