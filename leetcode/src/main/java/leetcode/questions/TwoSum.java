3279759883675f504cd8c24e83e03252c10d3a98
Cletus <clet.lee@gmail.com>
2016-04-09 10:22:48 +0800

    add the example test

---
+++ leetcode/src/main/java/leetcode/questions/TwoSum.java
@@ -0,0 +1,9 @@
+package leetcode.questions;
+
+public class TwoSum {
+
+	public int[] find(int[] nums) {
+		return null;
+	}
+
+}
--- leetcode/src/test/java/leetcode/questions/TestTwoSum.java
+++ leetcode/src/test/java/leetcode/questions/TestTwoSum.java
@@ -1,13 +1,27 @@
 package leetcode.questions;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 public class TestTwoSum {
+	
+	TwoSum twoSum;
+	
+	@Before
+	public void setUp() {
+		twoSum = new TwoSum();
+	}
 
 	@Test
-	public void firstTest() {
-		assertThat(1, is(1));
+	@Ignore
+	public void firstTwoSum() {
+		int nums[] = {2,7,11,15};
+		int target = 9;
+		
+		assertThat(twoSum.find(nums), is(new int[] {0,1}));
 	}
 }
