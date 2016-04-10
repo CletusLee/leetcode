7c2d030ab9462f7ba6da58f43c5ba039ebc68f57
Cletus <clet.lee@gmail.com>
2016-04-10 13:33:51 +0800

    finish add two numbers questions

--- leetcode/src/main/java/leetcode/questions/AddTwoNumbers.java
+++ leetcode/src/main/java/leetcode/questions/AddTwoNumbers.java
@@ -4,32 +4,51 @@ public class AddTwoNumbers {
 
 	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 		ListNode result = new ListNode(0);
-		ListNode cur = result;
+		ListNode resultCur = result;
 		while(true) {
-			int sum = l1.val + l2.val;
-			int mod = sum % 10;
-			int quo = sum / 10;
-			
-			cur.val += mod;
-			
-			if(quo != 0) {
-				cur.next = new ListNode(quo);
+			int sum = 0;
+			if(l1 != null) {
+				sum = l1.val;
 			}
 			
-			if(l1.next == null) {
+			if(l2 != null) {
+				sum += l2.val;
+			}
+			
+			sum += resultCur.val;
+			
+			int mod = sum % 10;
+			int carry = sum / 10;
+			
+			resultCur.val = mod;
+			
+			if(carry != 0) {
+				resultCur.next = new ListNode(carry);
+			}
+			
+			if(shouldStopComputing(l1, l2)) {
 				break;
 			} else {
-				if(cur.next == null) {
-					cur.next = new ListNode(0);			
+				if(resultCur.next == null) {
+					resultCur.next = new ListNode(0);			
 				}
 
-				cur = cur.next;
-				l1 = l1.next;
-				l2 = l2.next;
+				resultCur = resultCur.next;
+				if(l1 != null) {
+					l1 = l1.next;
+				}
+				
+				if(l2 != null) {
+					l2 = l2.next;
+				}
 			}
 		}
 		
 		return result;
+	}
+
+	private static boolean shouldStopComputing(ListNode l1, ListNode l2) {
+		return (l1 == null || l1.next == null) && (l2 == null || l2.next == null);
 	}
 	
 }
--- leetcode/src/test/java/leetcode/questions/TestAddTwoNumbers.java
+++ leetcode/src/test/java/leetcode/questions/TestAddTwoNumbers.java
@@ -29,4 +29,31 @@ public class TestAddTwoNumbers {
 		assertThat(result.next.val, is(0));
 		assertThat(result.next.next.val, is(8));
 	}
+	
+	@Test
+	public void testAddTwoNumbersWithDifferentLength() {
+		ListNode l1 = new ListNode(2,4,3);
+		ListNode l2 = new ListNode(5,6,4,5);
+		
+		ListNode result = AddTwoNumbers.addTwoNumbers(l1, l2);
+		
+		assertThat(result.val, is(7));
+		assertThat(result.next.val, is(0));
+		assertThat(result.next.next.val, is(8));
+		assertThat(result.next.next.next.val, is(5));
+		assertThat(result.next.next.next.next, is(nullValue()));
+	}
+	
+	@Test
+	public void abc() {
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(9,9);
+		
+		ListNode result = AddTwoNumbers.addTwoNumbers(l1, l2);
+		
+		assertThat(result.val, is(0));
+		assertThat(result.next.val, is(0));
+		assertThat(result.next.next.val, is(1));
+		assertThat(result.next.next.next, is(nullValue()));
+	}
 }
