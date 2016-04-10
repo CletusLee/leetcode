77e369d225750dd8cb7071f15a783b0de70576a8
Cletus <clet.lee@gmail.com>
2016-04-10 17:53:05 +0800

    finish longest substring without repeating characters

--- leetcode/src/main/java/leetcode/questions/LongestSubstringWithoutRepeatingCharacters.java
+++ leetcode/src/main/java/leetcode/questions/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,9 +1,36 @@
 package leetcode.questions;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class LongestSubstringWithoutRepeatingCharacters {
 
-	public static int lengthOfLongestSubstring(String string) {
-		return 3;
+	public static int lengthOfLongestSubstring(String s) {
+		if(s.length() == 0 ) {
+			return 0;
+		}
+		
+		Set<Character> foundChar = new HashSet<Character>();
+		ArrayList<Integer> results = new ArrayList<Integer>();
+		
+		for(int resultIndex = 0; resultIndex < s.length(); resultIndex++) {
+			results.add(0);
+			for(int curIndex = resultIndex; curIndex < s.length(); curIndex++) {
+				Character c = s.charAt(curIndex);
+				if(!foundChar.contains(c)) {
+					results.set(resultIndex, results.get(resultIndex) + 1);
+					foundChar.add(c);
+				} else {
+					foundChar.clear();
+					break;
+				}
+			}
+		}
+		
+		return Collections.max(results);
 	}
 
 }
--- leetcode/src/test/java/leetcode/questions/TestLongestSubstringWithoutRepeatingCharacters.java
+++ leetcode/src/test/java/leetcode/questions/TestLongestSubstringWithoutRepeatingCharacters.java
@@ -3,8 +3,6 @@ package leetcode.questions;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class TestLongestSubstringWithoutRepeatingCharacters {
@@ -15,4 +13,33 @@ public class TestLongestSubstringWithoutRepeatingCharacters {
 		
 		assertThat(length, is(3));
 	}
+	
+	@Test
+	public void testBbbbb() {
+		int length = LongestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring("bbbbb");
+		
+		assertThat(length, is(1));
+	}
+	
+	@Test
+	public void testZoneLengthInput() {
+		int length = LongestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring("");
+		
+		assertThat(length, is(0));
+	}
+	
+	@Test
+	public void testC() {
+		int length = LongestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring("c");
+		
+		assertThat(length, is(1));
+	}
+	
+	@Test
+	public void testAab() {
+		int length = LongestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring("aab");
+		
+		assertThat(length, is(2));
+	}
+
 }
