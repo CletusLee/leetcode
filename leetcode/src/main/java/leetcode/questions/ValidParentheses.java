88d7c0a3485c92d31135a98f186efd9511125f46
Cletus Lee <clet.lee@gmail.com>
2018-03-01 20:28:13 +0800

    Set up the basic layout for ValidParentheses problem.

---
+++ leetcode/src/main/java/leetcode/questions/ValidParentheses.java
@@ -0,0 +1,10 @@
+package leetcode.questions;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack operatorStack = new Stack();
+        return true;
+    }
+}
---
+++ leetcode/src/test/java/leetcode/questions/TestValidParentheses.java
@@ -0,0 +1,30 @@
+package leetcode.questions;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestValidParentheses {
+
+    ValidParentheses validParentheses;
+
+    @Before
+    public void setup() {
+        validParentheses = new ValidParentheses();
+    }
+
+    @Test
+    public void validParentheses() {
+        assertThat(validParentheses.isValid("()"), is(true));
+    }
+
+    @Test
+    public void invalidParentheses() {
+        assertThat(validParentheses.isValid("())"), is(false));
+    }
+
+}
